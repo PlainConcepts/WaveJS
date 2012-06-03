4cf0b8a80bfca9eb576db0aded307e7a2b8a8ae2
sarvaje <sarbaje@hotmail.com>
2012-06-03 03:47:59 +0200

    Add ray casting example

---
+++ models/cube.js
@@ -0,0 +1,18 @@
+function GetCube() {
+    var puzzlecube = new WaveEngineJS.Model();
+    puzzlecube.addVertex(39.3701, -39.3701, 39.3701);
+    puzzlecube.addVertex(39.3701, 39.3701, 39.3701);
+    puzzlecube.addVertex(-39.3701, 39.3701, 39.3701);
+    puzzlecube.addVertex(-39.3701, -39.3701, 39.3701);
+    puzzlecube.addVertex(39.3701, -39.3701, -39.3701);
+    puzzlecube.addVertex(39.3701, 39.3701, -39.3701);
+    puzzlecube.addVertex(-39.3701, -39.3701, -39.3701);
+    puzzlecube.addVertex(-39.3701, 39.3701, -39.3701);
+    puzzlecube.addPolygon([0, 1, 2, 3]);
+    puzzlecube.addPolygon([4, 5, 1, 0]);
+    puzzlecube.addPolygon([6, 7, 5, 4]);
+    puzzlecube.addPolygon([3, 2, 7, 6]);
+    puzzlecube.addPolygon([1, 5, 7, 2]);
+    puzzlecube.addPolygon([4, 0, 3, 6]);
+    return puzzlecube;
+}
